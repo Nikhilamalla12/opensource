243dddb513f248ed4e2406b7abf2b8464aa8f38c
Nikhilamalla12 <161217105+Nikhilamalla12@users.noreply.github.com>
2024-11-16 20:19:23 +0530

    Create On_Time_Arrival.py

---
+++ visaa_prep/On_Time_Arrival.py
@@ -0,0 +1,10 @@
+def main():
+    T=int(input())
+    for _ in range(T):
+        X=int(input())
+        if X>=30:
+            print("YES")
+        else:
+            print("NO")
+if __name__=="__main__":
+    main()

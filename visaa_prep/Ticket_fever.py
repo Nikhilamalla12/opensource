ffae041ebd6b91bee09464f17cb5e0172ef50817
Nikhilamalla12 <161217105+Nikhilamalla12@users.noreply.github.com>
2024-11-16 20:36:23 +0530

    Create Ticket_fever.py

---
+++ visaa_prep/Ticket_fever.py
@@ -0,0 +1,10 @@
+def main():
+    T=int(input())
+    for _ in range(T):
+        N, M = map(int, input().split())
+        if N>M:
+            print(N - M)
+        else:
+            print(0)
+if __name__=="__main__":
+    main()
